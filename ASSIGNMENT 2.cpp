98942b90ef056424ffea489198ad894968261c29
dodogood12 <dodogood12@gmail.com>
2015-05-22 21:34:01 +0100

    c++
    source for # assignment 1

---
+++ ASSIGNMENT 2.cpp
@@ -0,0 +1,25 @@
+// NAME:Dodo Emmanuel Abu
+// REG NO:451251776BF
+// DEPARTMENT:STATISTICS
+
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int x, y;
+	
+	cout << "Enter the two integers. \n";
+	cin >> x >> y;
+	
+	if (x % y == 0)
+	cout << "First is a multiple of the second. \n";
+	else
+	cout << "First is not a multiple of the second. \n";
+	
+	return 0;
+}
---
+++ ASSIGNMENT 4.cpp
@@ -0,0 +1,49 @@
+// NAME:Dodo Emmanuel Abu
+// REG NO:451251776BF
+// DEPARTMENT:STATISTICS
+
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+	     cout << "   ** \n"
+	         << " *    * \n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << " *    *\n"
+	         << "   **  \n";
+	     cout << "  *   \n"
+	          << " ***  \n"
+	          << "***** \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n";
+	     cout << "    *   \n"
+	          << "   * *    \n"
+	         << " *     *  \n"
+	        << "*       *\n"
+	        << " *     * \n"
+	        << "   * *   \n"
+	        << "    *    \n";
+	        
+	        return 0;
+}
